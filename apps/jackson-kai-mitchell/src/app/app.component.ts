38ab1ed8a333085dbdc8814799f58dffe3f1df0b
Charlie Greenman <charlie@razroo.com>
2024-01-31 02:13:01 -0800

    Add code for angular-17.0.0 create-workspace.
    [Url: razroo.com/community/angular-17.0.0/create-workspace-recipe/create-workspace]

---
+++ apps/jackson-kai-mitchell/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jackson-kai-mitchell-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'jackson-kai-mitchell';
+}
